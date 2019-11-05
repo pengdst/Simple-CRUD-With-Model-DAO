c6e1e624035426c8874aa01007c30c080acc0a5f
pengdst <penxkuh@gmail.com>
2019-11-05 15:13:21 +0700

    Bind View

--- app/src/main/java/com/example/simplecrudwithmodeldao/MainActivity.java
+++ app/src/main/java/com/example/simplecrudwithmodeldao/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.simplecrudwithmodeldao;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rvJadwal;
+    FloatingActionButton fabAdd;
+    Toolbar toolbar;
+    CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindView();
+
+    }
+
+    private void bindView() {
+        rvJadwal = findViewById(R.id.rv_jadwal);
+        fabAdd = findViewById(R.id.fab_add);
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
+        toolbar = findViewById(R.id.toolbar);
     }
 }
